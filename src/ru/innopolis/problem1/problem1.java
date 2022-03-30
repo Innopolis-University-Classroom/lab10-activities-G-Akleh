4734f130ed968db571af865456b6770b7608efed
G-Akleh <98668965+G-Akleh@users.noreply.github.com>
2022-03-30 18:04:16 +0300

    Update problem1.java

--- src/ru/innopolis/problem1/problem1.java
+++ src/ru/innopolis/problem1/problem1.java
@@ -5,27 +5,51 @@ package com.company;
 import java.util.*;
 
 public interface Shape {
-   int perimeter(){
-   }
+   float perimeter(){}
 }
+
 public class Rectangle implements Shape {
- @Override
- public int perimeter() {
+	private int height;
+	private int width;
+@Override
+ public float perimeter() {
+	 return 2 * (height + width);
  }
 }
+
 public class Sqaure implements Shape {
+	private int side;
  @Override
- public int perimeter() {
+ public float perimeter() {
+ 	return 4 * side;
  }
 }
+
 public class Circle implements Shape {
+	private int radius;
  @Override
- public int perimeter() {
+ public float perimeter() {
+	 return 2 * 3.14 * radius;
  }
+}
+
+public class ShapeFactory {
+ //use getShape method to get object of type shape
+	public Shape getShape(String shapeType){
+ 		if(shapeType == null){
+ 			return null;
+ 		} else if (shapeType == "Circle") {
+			return Circle();
+		} else if (shapeType == "Square") {
+			return Square();
+		} else if (shapeType == "Rectangle") {
+			return Rectangle();
+		}
+	}
 }
 
 public class Main {
 	public static void main(String[] args) {
-
+		
 	}
 }
