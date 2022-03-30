44c94f009c6a1377d297e9762c0ecad088e057e9
G-Akleh <98668965+G-Akleh@users.noreply.github.com>
2022-03-30 17:35:17 +0300

    Update problem1.java

--- src/ru/innopolis/problem1/problem1.java
+++ src/ru/innopolis/problem1/problem1.java
@@ -1,2 +1,31 @@
 // your solution here
 //fix all the errors including package errors
+package com.company;
+
+import java.util.*;
+
+public interface Shape {
+   int perimeter(){
+   }
+}
+public class Rectangle implements Shape {
+ @Override
+ public int perimeter() {
+ }
+}
+public class Sqaure implements Shape {
+ @Override
+ public int perimeter() {
+ }
+}
+public class Circle implements Shape {
+ @Override
+ public int perimeter() {
+ }
+}
+
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
